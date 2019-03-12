13562069f5c16b9da784c7a22fb1170404f5bc65
Alice(BoyarchenkoEE) <egor4ik@mprei.ru>
2019-03-12 14:50:42 +0300

    code: заготовка программы

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
+main()
 {
-   cout << "Enter A and B: ";
+    cout << "Enter A and B: ";
 int a, b;
 cin >> a >> b;
-cout << "A + B = " << a + b << '\n';
+cout << "A + B = " << a + b << '\n'
+     << "A - B = " << a - b << '\n';
 }
