f9cb1c49cba95430e2929b1f1cf7ada27c3fd1f8
Alice(BoyarchenkoEE) <egor4ik@mprei.ru>
2019-03-12 14:52:46 +0300

    Vvod

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
+#include <iostream>
+using namespace std;
 main()
 {
-    cout << "Enter A and B: ";
+cout << "Enter A and B: ";
 int a, b;
 cin >> a >> b;
-cout << "A + B = " << a + b << '\n'
-     << "A - B = " << a - b << '\n';
 }
